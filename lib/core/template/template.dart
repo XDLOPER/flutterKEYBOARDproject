bed40c45481248a06cf0c6a77d16052adc603de2
XDLOPER <yasirkaygusuzone@hotmail.com>
2023-01-01 05:09:34 +0300

    update template

--- lib/core/template/template.dart
+++ lib/core/template/template.dart
@@ -1,42 +1,73 @@
-import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter/src/widgets/container.dart';
-import 'package:flutter/src/widgets/framework.dart';
+import 'package:flutter/services.dart';
+import 'package:icons_plus/icons_plus.dart';
 
 import '../../main.dart';
 
 class HOME extends StatelessWidget {
-  const HOME({super.key});
+  HOME({super.key});
+  final _scaffoldKey = GlobalKey<ScaffoldState>();
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: themeAppBar(context),
-      body: themeAppBody(context),
+      key: _scaffoldKey,
+      appBar: _themeAppBar(context),
+      body: _themeAppBody(context),
+      drawer: _drawer(context),
     );
   }
 
-  Widget themeAppBody(BuildContext context) {
+  Drawer _drawer(BuildContext context) {
+    return Drawer(
+      child: ListView(children: [
+        ListTile(
+          title: Text('setting', style: Theme.of(context).textTheme.headline6),
+          leading: Icon(BoxIcons.bx_cog),
+          onTap: (() {
+            Navigator.pop(context);
+          }),
+        ),
+        ListTile(
+          title: Text('exit', style: Theme.of(context).textTheme.headline6),
+          leading: Icon(BoxIcons.bx_exit),
+          onTap: (() {
+            SystemNavigator.pop();
+          }),
+        )
+      ]),
+    );
+  }
+
+  Widget _themeAppBody(BuildContext context) {
     return const Center(child: Text('redadwa'));
   }
 
-  AppBar themeAppBar(BuildContext context) {
+  AppBar _themeAppBar(BuildContext context) {
     return AppBar(
-      title: Text(MyApp.appTitle),
+      title: Row(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: const [
+          Padding(
+            padding: EdgeInsets.only(left: 10, right: 10),
+            child: Text(
+              'xastral',
+              style: TextStyle(
+                  fontSize: 25, fontWeight: FontWeight.bold, letterSpacing: 5),
+            ),
+          ),
+          SizedBox(width: 10, height: 10, child: CircularProgressIndicator())
+        ],
+      ),
       centerTitle: true,
       backgroundColor: Colors.transparent,
       elevation: 0,
-      leading: const Icon(Icons.ac_unit),
+      leading: Builder(
+          builder: (BuildContext context) => IconButton(
+              icon: Icon(Icons.more_horiz),
+              onPressed: () => Scaffold.of(context).openDrawer())),
       actions: [
-        const Center(
-            child: SizedBox(
-          width: 10,
-          height: 10,
-          child: CircularProgressIndicator(),
-        )),
-        IconButton(
-            onPressed: (() => false),
-            icon: Icon(Icons.subdirectory_arrow_left)),
+        IconButton(onPressed: (() => false), icon: Icon(Icons.memory_outlined)),
       ],
     );
   }
--- lib/main.dart
+++ lib/main.dart
@@ -17,6 +17,7 @@ class MyApp extends StatelessWidget {
           primarySwatch: Colors.lightBlue,
           appBarTheme: const AppBarTheme(backgroundColor: Colors.redAccent)),
       home: HOME(),
+      
     );
   }
 }
